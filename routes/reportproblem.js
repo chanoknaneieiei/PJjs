37d31ce4096f6917a6df115296dcdca67bc501dc
Benchaphol <6306021620085@fitm.kmutnb.ac.th>
2022-04-23 14:26:34 +0700

    update

---
+++ routes/reportproblem.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+const { check, validationResult } = require('express-validator');
+
+const monk = require('monk')("localhost:27017/ProjectJS");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render("reportproblem");
+});
+
+router.get('/success', function(req, res, next) {
+    res.render("reportproblem");
+});
+
+router.post('/success', [
+    check("problem" , "กรุณาป้อนปัญหา").not().isEmpty()
+],function(req, res, next) {
+    const result = validationResult(req);
+    var errors = result.errors;
+    if (!result.isEmpty()) {
+        res.render('reportproblem' , {errors : errors});
+    }
+    else{
+        //insert data to DB
+        var collection = monk.get('problem');
+        collection.insert({
+            problem:req.body.problem
+        } , function(err , data){
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.location('/');
+                res.redirect('/');
+
+            }
+        });
+    }
+});
+
+module.exports = router;
