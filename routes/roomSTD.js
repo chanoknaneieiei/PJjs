13dc06db55a402e6b090ad52ab43f0e1796ea1ac
Benchaphol <6306021620085@fitm.kmutnb.ac.th>
2022-04-25 19:16:21 +0700

    update

--- routes/roomSTD.js
+++ routes/roomSTD.js
@@ -5,24 +5,4 @@ router.get('/', function(req, res, next) {
   res.render('roomSTD');
 });
 
-router.get('/floor1', function(req, res, next) {
-  res.render('f1Std');
-});
-
-router.get('/floor2', function(req, res, next) {
-  res.render('f2Std');
-});
-
-router.get('/floor3', function(req, res, next) {
-  res.render('f3Std');
-});
-
-router.get('/floor4', function(req, res, next) {
-  res.render('f4Std',);
-});
-
-router.get('/floor5', function(req, res, next) {
-  res.render('f5Std');
-});
-
 module.exports = router;
